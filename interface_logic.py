15ff808cb270dc836df47646f9352b82c60e2bc3
Matthias <matthias.wellstein@web.de>
2020-02-14 15:57:26 +0100

    Handle check_queue directly

--- interface_logic.py
+++ interface_logic.py
@@ -4,6 +4,8 @@ from log_collector import LogCollector
 from uploader import Uploader
 from user_interface import UserInterface
 from tkinter import filedialog
+from typing import List
+
 
 ui: UserInterface
 
@@ -35,43 +37,34 @@ def _click_upload():
     global uploaded_logs, upload_queue
     if uploaded_logs and upload_thread and upload_queue:
         _reset()
-    ui.startButton.configure(text="Collecting")
+    ui.uploadBtn.configure(text="Collecting")
     raid_days = [day for i, day in enumerate(ui.weekdays) if ui.weekdaysVar[i].get()]
     logs = LogCollector(ui.logPath.get(), raid_days, ui.week_delta.get(), 200000, ui.fracVar.get())
     logs = logs.collect()
 
     if not logs:
         # Finish, since nothing was uploaded
-        ui.progress["maximum"] = 1
-        ui.progress["value"] = 1
-        ui.startButton["text"] = "No logs found."
+        ui.uploadPrg["maximum"] = 1
+        ui.uploadPrg["value"] = 1
+        ui.uploadBtn["text"] = "No logs found."
     else:
         # Set up the progressbar
-        ui.progress["value"] = 0
-        ui.progress["maximum"] = len(logs)
+        ui.uploadPrg["value"] = 0
+        ui.uploadPrg["maximum"] = len(logs)
         # Start the Log Upload
-        ui.startButton.configure(text="Uploading")
+        ui.uploadBtn.configure(text="Uploading")
         uploaded_logs = []
         upload_queue = Queue(len(logs))
         up = Uploader(upload_queue)
         up.parallel_upload(logs)
-        check_queue()
-
-
-def check_queue():
-    try:
-        uploaded_logs.append(str(upload_queue.get(block=False)))
-    except Empty:
-        pass
-    else:
-        ui.progress["value"] += 1
-    if not upload_thread.is_alive():
-        ui.startButton.configure(text="Done")
-    ui.after(1000, check_queue)
+        for _ in range(len(logs)):
+            uploaded_logs.append(str(upload_queue.get()))
+            ui.uploadPrg["value"] += 1
+        ui.uploadBtn.configure(text="Done")
 
 
 def _reset():
-    ui.startButton.configure(text="Start Upload")
+    ui.uploadBtn.configure(text="Start Upload")
     ui.copyButton.configure(text="Copy to Clipboard")
     global uploaded_logs, upload_thread, upload_queue
     uploaded_logs.clear()
