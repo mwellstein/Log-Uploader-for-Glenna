21f516cff083386a14e915ad9ff0014f2d2c13a8
Matthias <matthias.wellstein@web.de>
2020-02-16 16:23:22 +0100

    Commentary

--- interface_logic.py
+++ interface_logic.py
@@ -1,5 +1,10 @@
+"""
+Handle the inputs from the user interface.
+All functions (mostly clicks and help functions) are down below.
+Only design changes will be label text and progressbar.
+"""
 from queue import Queue, Empty
-from threading import Thread, active_count
+from threading import Thread
 from tkinter import filedialog, messagebox
 from typing import List
 
@@ -8,20 +13,33 @@ from log_collector import LogCollector
 from uploader import Uploader
 from user_interface import UserInterface
 
+# To handle changes and variables of the ui, without circle import, assign the ui from user_interface
 ui: UserInterface
 
+# The current used container to collect the logs, the active thread that manages uploading
+# and the queue to retrieve upload information
 uploaded_logs = []
 upload_thread = Thread()
 upload_queue = Queue()
+# After id is used to cancel ui.after(), which otherwise continues to call the given function
+# Kinda unclear, since it shouldn't just be recursive calling, but calling it once only didn't work out
 after_id = 0
 
 
-def logic_ui(gui):
+def logic_ui(gui) -> None:
+    """
+    Set the UI to apply the logic to. Just a setter.
+    :param gui: The gui-root after calling it in user_interfaces __main__ part
+    """
     global ui
     ui = gui
 
 
-def click_upload():
+def click_upload() -> None:
+    """
+    Handles the click to start upload. Opens a new thread to handle everything related to the upload.
+    This new thread becomes the global upload_thread
+    """
     t = Thread(target=_click_upload)
     t.daemon = True
     global upload_thread
@@ -29,7 +47,10 @@ def click_upload():
     t.start()
 
 
-def _click_upload():
+def _click_upload() -> None:
+    """
+    Called in upload_thread, disables button, than collects all relevant setting in the ui and collects relevant logs.
+    """
     ui.uploadBtn.configure(text="Collecting", state="disabled")
     raid_days = [day for i, day in enumerate(ui.weekdays) if ui.weekdaysVar[i].get()]
     logs = LogCollector(ui.logPath.get(), raid_days, ui.week_delta.get(), 200000, ui.fracVar.get())
@@ -48,7 +69,12 @@ def _click_upload():
         _start_upload(logs)
 
 
-def _start_upload(logs: List[Log]):
+def _start_upload(logs: List[Log]) -> None:
+    """
+    Setting up the queue and Uploader instance. Than starts uploading and initialize checking of the queue.
+    Not part of _click_upload to enable call from on_upload_failure with the remaining logs.
+    :param logs: The logs that shall get uploaded by the uploader instance
+    """
     ui.uploadBtn.configure(text="Uploading")
     global uploaded_logs, upload_queue
     uploaded_logs = []
@@ -58,8 +84,14 @@ def _start_upload(logs: List[Log]):
     check_queue(up, len(logs))
 
 
-def check_queue(up: Uploader, logs_len: int):
-    print(active_count())
+def check_queue(up: Uploader, logs_len: int) -> None:
+    """
+    To have a working progressbar and kinda error handling from the upload subthreads the queue will be checked
+    every second if it's filled. If not ignore, else update progressbar.
+    Check if all logs got uploaded or if an error happened during the upload. Lastly call again after 1 second
+    :param up: The Uploader instance to give it as kinda error handle for on_failure
+    :param logs_len: The amount of logs that shall be uploaded to check for completing of the job
+    """
     try:
         uploaded_logs.append(str(upload_queue.get(block=False)))
     except Empty:
@@ -68,7 +100,7 @@ def check_queue(up: Uploader, logs_len: int):
         ui.uploadPrg["value"] += 1
 
     if len(uploaded_logs) == logs_len:
-        ui.uploadBtn.configure(text="Done")
+        ui.uploadBtn.configure(text="Done", state="normal")
     elif up.failed:
         on_upload_failure(up)
     else:
@@ -76,7 +108,11 @@ def check_queue(up: Uploader, logs_len: int):
         after_id = ui.after(1000, check_queue, up, logs_len)
 
 
-def on_upload_failure(up: Uploader):
+def on_upload_failure(up: Uploader) -> None:
+    """
+    If a fail was detected, collect all fails, ask for retry. If not, reset program
+    :param up: The Uploader instance, contains which logs failed
+    """
     fail_string = ""
     for fail in up.failed_logs:
         fail_string += f"{fail.boss} "
@@ -86,13 +122,19 @@ def on_upload_failure(up: Uploader):
         _reset()
 
 
-def _reset():
+def _reset() -> None:
+    """
+    As much reset as possible. Can't terminate threads easily, thus tried to cancel them by themself
+    """
     ui.uploadBtn.configure(text="Start Upload", state="normal")
     ui.copyButton.configure(text="Copy to Clipboard")
     ui.after_cancel(after_id)
 
 
-def click_copy():
+def click_copy() -> None:
+    """
+    Clears the Clipboard, then adds all links and try_count as new lines.
+    """
     ui.clipboard_clear()
     for log_line in uploaded_logs:
         ui.update()
@@ -100,5 +142,8 @@ def click_copy():
     ui.copyButton.configure(text="Copied")
 
 
-def click_browse():
+def click_browse() -> None:
+    """
+    Open up filedialog to get main log directory
+    """
     ui.logPath.set(filedialog.askdirectory())
