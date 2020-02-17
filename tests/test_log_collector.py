1a733f12e9a3756440b143ac01cc96eba9c988e1
Matthias <matthias.wellstein@web.de>
2020-02-17 22:48:31 +0100

    Mock LogCollector test (no content yet)

---
+++ tests/test_log_collector.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestLogCollector(unittest.TestCase):
+    def test_stats(self):
+        # Test from_ to path name etc.
+        pass
+
+    def test_collection(self):
+        # Test if files get collected correctly
+        pass
+
+    def test_filter(self):
+        # Test if the filter returns relevant logs
+        pass
